89aa48fac9b0af119e6408845f950ed4bcb2624b
Nicolas Rossitto <nicolas.rossitto23@gmail.com>
2023-03-21 13:33:44 +0100

    add getOne for sales

--- controllers/sales.controller.js
+++ controllers/sales.controller.js
@@ -4,6 +4,21 @@ const salesController = {
     getAll: async (req, res) => {
         let allSales = await salesService.getAll();
         res.json(allSales);
+    },
+
+    getOne: async (req, res, next) => {
+        let id = req.params.id;
+
+        let oneSale = await salesService.getOne(id);
+
+        if (oneSale.id != undefined) {
+            res.json(oneSale);
+        } else if (oneSale.errorMessage != undefined) {
+            res.locals.message = oneSale;
+            next();
+        } else {
+            throw new Error("Une erreur est survenue ...");
+        }
     }
 };
 
--- models/sales.models.js
+++ models/sales.models.js
@@ -8,6 +8,27 @@ const salesModels = {
         const result = await db.query("select * from sales");
         db.close();
         return result.recordset;
+    },
+
+    getOne: async (id) => {
+        let db;
+        try {
+            db = await getDbConnection();
+            const querySQL = 'SELECT * FROM sales where id = @id';
+
+            const request = new mssql.Request(db);
+            request.input('Id', mssql.Int, id);
+
+            const result = await request.query(querySQL);
+
+            if(result.recordset.length !== 1) {
+                return null;
+            }
+            return result.recordset[0];
+        }
+        finally {
+            db?.close();
+        }
     }
 };
 
